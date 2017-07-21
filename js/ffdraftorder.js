bebca20189a49f827c9d13baed0fa3071f762abb
Joey Pellegrino <joepell19@gmail.com>
2017-07-21 09:14:28 -0700

    Update to final standings

--- js/ffdraftorder.js
+++ js/ffdraftorder.js
@@ -2,7 +2,9 @@
 
 //alert("Proceed to display draft order for 2017.");
 
-var teams = ['Team7', 'Team8', 'Team9', 'Team10', 'Team11', 'Team12', 'Team13', 'Team14'];
+var teams = ['Jared Nelson', 'Nick Routh', 'Max Nelson', 'Alex', 'Krista Bates', 'Max Clinch', 'Bryce Hairabedian', 'Ryan Silva'];
+
+var playoffTeams = ['Gianni', 'Braden', 'Brian', 'Courtney', 'Joey', 'Akis']
 
 var team7 = true;
 var team8 = true;
@@ -13,14 +15,14 @@ var team12 = true;
 var team13 = true;
 var team14 = true;
 
-var draftPosition = ["1st: ", "2nd: ", "3rd: ", "4th: ", "5th: ", "6th: ", "7th: "];
+var draftPosition = ["1st: ", "2nd: ", "3rd: ", "4th: ", "5th: ", "6th: ", "7th: ", "8th: "];
 var currentPosition = 0;
 
 while (team7 == true || team8 == true || team9 == true || team10 == true || team11 == true || team12 == true || team13 == true || team14 == true){
     //alert current draft position
     //needs more here to display current position and then display team picked
     alert(draftPosition[currentPosition]);
-    currentPosition++;
+    
     
     //random # to pick team
     var draftPercentage = Math.random();
@@ -37,47 +39,55 @@ while (team7 == true || team8 == true || team9 == true || team10 == true || team
         document.getElementById("demo").appendChild(newList);
         //team cannot be chosen again
         team7 = false;
+        currentPosition++;
     } else if (draftPercentage >.25 && draftPercentage <= .43 && team8 == true){
         console.log(teams[1]);
         var newList = document.createElement("li");
         newList.innerHTML = teams[1];
         document.getElementById("demo").appendChild(newList);
         team8 = false;
+        currentPosition++;
     } else if (draftPercentage >.43 && draftPercentage <= .58 && team9 == true){
         console.log(teams[2]);
         var newList = document.createElement("li");
         newList.innerHTML = teams[2];
         document.getElementById("demo").appendChild(newList);
         team9 = false;
+        currentPosition++;
     } else if (draftPercentage >.58 && draftPercentage <= .71 && team10 == true){
         console.log(teams[3]);
         var newList = document.createElement("li");
         newList.innerHTML = teams[3];
         document.getElementById("demo").appendChild(newList);
         team10 = false;
+        currentPosition++;
     } else if (draftPercentage >.71 && draftPercentage <= .83 && team11 == true){
         console.log(teams[4]);
         var newList = document.createElement("li");
         newList.innerHTML = teams[4];
         document.getElementById("demo").appendChild(newList);
         team11 = false;
+        currentPosition++;
     } else if (draftPercentage >.83 && draftPercentage <= .93 && team12 == true){
         console.log(teams[5]);
         var newList = document.createElement("li");
         newList.innerHTML = teams[5];
         document.getElementById("demo").appendChild(newList);
         team12 = false;
+        currentPosition++;
     } else if (draftPercentage >.93 && draftPercentage <= .98 && team13 == true){
         console.log(teams[6]);
         var newList = document.createElement("li");
         newList.innerHTML = teams[6];
         document.getElementById("demo").appendChild(newList);
         team13 = false;
+        currentPosition++;
     } else if (draftPercentage >.98 && draftPercentage < 1 && team14 == true){
         console.log(teams[7]);
         var newList = document.createElement("li");
         newList.innerHTML = teams[7];
         document.getElementById("demo").appendChild(newList);
         team14 = false;
+        currentPosition++;
     }
 }
